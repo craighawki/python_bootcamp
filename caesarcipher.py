52af23f0cbb61560b6bde6f4e330b18858c536df
Craig Hawkins <craighawki@gmail.com>
2021-06-19 00:44:13 -0700

    code for going off the end of the list

--- caesarcipher.py
+++ caesarcipher.py
@@ -4,22 +4,26 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n\n"))
+shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    word = ''
-    if direction == 'encrypt':
-        for char in text:
+def encrypt(plain_text, shift_amount):
+    encoded_word = ''
+    if direction == 'encode':
+        for char in plain_text:
         #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-            char = (int(alphabet.index(char)) + shift)
-            let = (alphabet[char])
-            word += let
-    print(word)
-           
+            char = (int(alphabet.index(char)) + shift_amount)
+            if char <= 25:
+                let = (alphabet[char])
+                encoded_word += let
+            else:
+                let = (alphabet[char - 25])
+                encoded_word += let
+    print(f"The encoded text is {encoded_word}")    
         
 
-encrypt(text, shift)  
+  
+
     #e.g. 
     #plain_text = "hello"
     #shift = 5
@@ -32,3 +36,4 @@ encrypt(text, shift)
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(plain_text=text, shift_amount=shift)
