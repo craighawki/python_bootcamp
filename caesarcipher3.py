4e9ea47ef63616c085b7a634d63c459586bf002c
Craig Hawkins <craighawki@gmail.com>
2021-06-24 20:04:02 -0700

    more decide fixup

--- caesarcipher3.py
+++ caesarcipher3.py
@@ -13,9 +13,8 @@ def caesar(start_text, shift_amount, cipher_direction):
   for letter in start_text:
     position = alphabet.index(letter) 
     new_position = position + shift_amount
-    print(shift_amount)
     end_text += alphabet[new_position]    
-    print(f"The {cipher_direction}d text is {end_text}")
+  print(f"The {cipher_direction}d text is {end_text}")
 
 
 
